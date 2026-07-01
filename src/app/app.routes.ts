4c00e3fea27336a07d86356fde07d8af249bffbe
Issac Rodriguez <issac.rodriguez@10648104L032.unah.edu>
2026-07-01 11:00:29 -0600

    subiolt

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Detalle } from './pages/detalle/detalle';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    
+
+
+    
+];
